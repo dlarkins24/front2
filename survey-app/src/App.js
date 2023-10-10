9f411ffc7b11b8c24d84aac9fa0e43c6b507b066
dlarkins24 <137289283+dlarkins24@users.noreply.github.com>
2023-10-10 20:17:18 +0000

    local session

--- survey-app/src/App.js
+++ survey-app/src/App.js
@@ -5,18 +5,27 @@ import Phase1Questions from './Phase1Questions';
 import Phase1Scores from './Phase1Scores';
 
 function App() {
-    const [sessionId, setSessionId] = useState(null);
+    // Initialize sessionId with a value from localStorage
+    const [sessionId, setSessionId] = useState(() => localStorage.getItem('sessionId'));
 
     const handleSessionStart = (id) => {
+        // Update state and localStorage when sessionId changes
         setSessionId(id);
+        localStorage.setItem('sessionId', id);
     };
 
     return (
         <Router>
             <Routes>
                 <Route path="/welcome" element={<WelcomePage onSessionStart={handleSessionStart} />} />
-                <Route path="/phase1-questions" element={sessionId ? <Phase1Questions sessionId={sessionId} /> : <Navigate to="/welcome" />} />
-                <Route path="/Phase1Scores" element={sessionId ? <Phase1Scores sessionId={sessionId} /> : <Navigate to="/welcome" />} />
+                <Route 
+                    path="/phase1-questions" 
+                    element={sessionId ? <Phase1Questions sessionId={sessionId} /> : <Navigate to="/welcome" />} 
+                />
+                <Route 
+                    path="/Phase1Scores" 
+                    element={sessionId ? <Phase1Scores sessionId={sessionId} /> : <Navigate to="/welcome" />} 
+                />
                 {/* Default route */}
                 <Route path="/" element={<Navigate to="/welcome" />} />
                 <Route path="*" element={<Navigate to="/welcome" />} /> {/* Catch all route */}
