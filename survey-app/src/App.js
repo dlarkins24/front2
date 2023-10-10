91c5dccea3a7956ab56f475c9f5b3df685da102f
dlarkins24 <137289283+dlarkins24@users.noreply.github.com>
2023-10-10 19:58:30 +0000

    stage 3

--- survey-app/src/App.js
+++ survey-app/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
-import Phase1Questions from './Phase1Questions'; 
+import Phase1Questions from './Phase1Questions';
+import Stage3 from './Stage3';
 
 function App() {
     const [sessionId, setSessionId] = useState(null);
@@ -15,6 +16,7 @@ function App() {
             <Routes>
                 <Route path="/welcome" element={<WelcomePage onSessionStart={handleSessionStart} />} />
                 <Route path="/phase1-questions" element={sessionId ? <Phase1Questions sessionId={sessionId} /> : <Navigate to="/welcome" />} />
+                <Route path="/stage3" element={sessionId ? <Stage3 sessionId={sessionId} /> : <Navigate to="/welcome" />} />
                 {/* Default route */}
                 <Route path="/" element={<Navigate to="/welcome" />} />
                 <Route path="*" element={<Navigate to="/welcome" />} /> {/* Catch all route */}
