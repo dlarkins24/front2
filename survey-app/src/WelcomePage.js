d0d4c12cc37959bd9a1954b667739aaa06c57c68
dlarkins24 <137289283+dlarkins24@users.noreply.github.com>
2023-11-08 15:57:32 +0000

    welcome try 2

--- survey-app/src/WelcomePage.js
+++ survey-app/src/WelcomePage.js
@@ -13,26 +13,19 @@ const WelcomePage = ({ onSessionStart }) => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const fetchIndustries = async () => {
+        const fetchData = async () => {
             try {
-                const response = await axios.get('https://back2.azurewebsites.net/get-industries');
-                setIndustries(response.data.industries);
+                const industriesResponse = await axios.get('https://back2.azurewebsites.net/get-industries');
+                setIndustries(industriesResponse.data.industries);
+
+                const orgSizesResponse = await axios.get('https://back2.azurewebsites.net/get-org-sizes');
+                setOrgSizes(orgSizesResponse.data.orgSizes);
             } catch (error) {
-                setError("Error fetching industries. Please try again later.");
+                setError("Error fetching data. Please try again later.");
             }
         };
 
-        const fetchOrgSizes = async () => {
-            try {
-                const response = await axios.get('https://back2.azurewebsites.net/get-org-sizes');
-                setOrgSizes(response.data.orgSizes);
-            } catch (error) {
-                setError("Error fetching organization sizes. Please try again later.");
-            }
-        };
-
-        fetchIndustries();
-        fetchOrgSizes();
+        fetchData();
     }, []);
 
     const handleIndustryChange = (e) => {
@@ -47,11 +40,16 @@ const WelcomePage = ({ onSessionStart }) => {
         try {
             setLoading(true);
             setError(null);
-            const response = await axios.post('https://back2.azurewebsites.net/start-session', {
+            const sessionResponse = await axios.post('https://back2.azurewebsites.net/start-session');
+            const sessionId = sessionResponse.data.sessionId;
+
+            // Send the welcome responses to the backend
+            await axios.post('https://back2.azurewebsites.net/submit-welcome-responses', {
+                sessionId: sessionId,
                 industry: selectedIndustry,
                 orgSize: selectedOrgSize
             });
-            const sessionId = response.data.sessionId;
+
             onSessionStart(sessionId);
             navigate('/Phase1Questions');
         } catch (error) {
@@ -65,26 +63,29 @@ const WelcomePage = ({ onSessionStart }) => {
     return (
         <div className="app-container">
             <div className="welcome-container">
-                {/* existing content */}
-                <div className="dropdown-container">
-                    <div className="input-group">
-                        <label htmlFor="industry">Industry:</label>
-                        <select name="industry" value={selectedIndustry} onChange={handleIndustryChange} required>
-                            <option value="" disabled>Select your industry</option>
-                            {industries.map(industry => (
-                                <option key={industry} value={industry}>{industry}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="orgSize">Organization Size:</label>
-                        <select name="orgSize" value={selectedOrgSize} onChange={handleOrgSizeChange} required>
-                            <option value="" disabled>Select your organization size</option>
-                            {orgSizes.map(size => (
-                                <option key={size} value={size}>{size}</option>
-                            ))}
-                        </select>
-                    </div>
+                {/* Logo and welcome title elements */}
+                {/* ... other elements ... */}
+
+                {/* Dropdown for selecting Industry */}
+                <div className="input-group">
+                    <label htmlFor="industry">Industry:</label>
+                    <select name="industry" value={selectedIndustry} onChange={handleIndustryChange} required>
+                        <option value="" disabled>Select your industry</option>
+                        {industries.map(industry => (
+                            <option key={industry} value={industry}>{industry}</option>
+                        ))}
+                    </select>
+                </div>
+
+                {/* Dropdown for selecting Organization Size */}
+                <div className="input-group">
+                    <label htmlFor="orgSize">Organization Size:</label>
+                    <select name="orgSize" value={selectedOrgSize} onChange={handleOrgSizeChange} required>
+                        <option value="" disabled>Select your organization size</option>
+                        {orgSizes.map(size => (
+                            <option key={size} value={size}>{size}</option>
+                        ))}
+                    </select>
                 </div>
 
                 <button 
